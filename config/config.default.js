a869a3a812ea45fe4133dca3d1e0fa7efd873ad6
985ch <115839257@qq.com>
2019-12-14 23:29:53 +0800

    允许跨域请求

--- config/config.default.js
+++ config/config.default.js
@@ -15,6 +15,13 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1562342251296_1465';
 
+  // 加密设置
+  config.security = {
+    csrf: false,
+    ctoken: false,
+    domainWhiteList: [ 'localhost:9527' ], // 允许跨域的白名单,为false时不限制跨域
+  };
+
   config.view = {
     defaultViewEngine: 'ejs',
     mapping: {
